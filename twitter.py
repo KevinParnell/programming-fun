660d4ad988b2553d4e6400ffc152b4e0f013410b
Kevin Parnell <kpbballer@gmail.com>
2016-10-14 10:37:32 -0400

    added back broken twitter program

--- twitter.py
+++ twitter.py
@@ -1,23 +1,13 @@
+# Broken twitter program I added back cause I can
 import tweepy
 
-def get_api(cfg):
-  auth = tweepy.OAuthHandler(cfg['k16Mu64PQbDP7scV77x7gAj91'], cfg['dbP9TSCRWRNonw8CmMRiotkoY5RR8QkeirsrJdl4Y6LZYMuZUr'])
-  auth.set_access_token(cfg['785888278666813440-ERjkfXZXXeu1vzy8HuXc0Nc4E1bKeMI'], cfg['FosmxFb3QxCLDr4rsKX0LPYrjTh6bq6XU93147eHOQHps'])
-  return tweepy.API(auth)
+CONSUMER_KEY = "k16Mu64PQbDP7scV77x7gAj91"
+CONSUMER_SECRET = "dbP9TSCRWRNonw8CmMRiotkoY5RR8QkeirsrJdl4Y6LZYMuZUr"
+ACCESS_KEY = "785888278666813440-ERjkfXZXXeu1vzy8HuXc0Nc4E1bKeMI"
+ACCESS_SECRET = "FosmxFb3QxCLDr4rsKX0LPYrjTh6bq6XU93147eHOQHps"
 
-def main():
-  # Fill in the values noted in previous step here
-  cfg = {
-    "consumer_key"        : "k16Mu64PQbDP7scV77x7gAj91",
-    "consumer_secret"     : "dbP9TSCRWRNonw8CmMRiotkoY5RR8QkeirsrJdl4Y6LZYMuZUr",
-    "access_token"        : "785888278666813440-ERjkfXZXXeu1vzy8HuXc0Nc4E1bKeMI",
-    "access_token_secret" : "FosmxFb3QxCLDr4rsKX0LPYrjTh6bq6XU93147eHOQHps"
-    }
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 
-  api = get_api(cfg)
-  tweet = "Good Afternoon!"
-  status = api.update_status(status=tweet)
-  # Yes, tweet is called 'status' rather confusing
-
-if __name__ == "__main__":
-  main()
+api = tweepy.API(auth)
+api.update_status('Good Morning!')
